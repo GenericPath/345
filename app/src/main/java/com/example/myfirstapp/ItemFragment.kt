0ba2c1fc180f6ea209c49111a889f978474b3ad1
Garth <walga757@student.otago.ac.nz>
2020-05-27 23:07:59 +1200

    Begin web parsing, doesnt fully work

--- app/src/main/java/com/example/myfirstapp/ItemFragment.kt
+++ app/src/main/java/com/example/myfirstapp/ItemFragment.kt
@@ -1,38 +1,68 @@
 package com.example.myfirstapp
 
+import android.os.AsyncTask
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_item_list.*
+import org.jsoup.Jsoup
+import org.jsoup.nodes.*
+import org.jsoup.select.Elements
+import java.lang.Exception
+import java.lang.ref.WeakReference
+
 
 class ItemFragment : AppCompatActivity() {
+    var elements : Elements = Elements()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_item_list)
 
-        val list = generateList(500)
+        val task : AsyncTask<Void,Void,Void> = LinkListTask("https://www.cs.otago.ac.nz/cosc244/lectures.php").execute()
+        val list = generateList(elements)
+
 
         recycler_view.adapter = MyItemRecyclerViewAdapter(list)
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
     }
 
-    private fun generateList(size: Int): List<TestItem> {
+    private fun generateList(elements : Elements): List<TestItem> {
 
         val list = ArrayList<TestItem>()
 
-        for (i in 0 until size) {
+        for (i in 0 until elements.size) {
             val drawable = when (i % 3) {
                 0 -> R.drawable.ic_android
                 1 -> R.drawable.ic_sentiment
                 else -> R.drawable.ic_thumb
             }
 
-            val item = TestItem(drawable, "Item $i", "Line 2")
+            val item = TestItem(drawable, "Lecture $i", text2 = elements[i].attr("href"))
             list += item
         }
 
         return list
     }
+
+    inner class LinkListTask(private val url : String) : AsyncTask<Void, Void, Void>() {
+
+        override fun doInBackground(vararg params: Void?): Void? {
+            // Get the html document from the url
+            try {
+                val document: Document = Jsoup.connect(url).get()
+                elements = document.select("tr a")
+            } catch (e : Exception) {
+                e.printStackTrace()
+            }
+
+
+            return null
+        }
+    }
+
 }
+
+
+
