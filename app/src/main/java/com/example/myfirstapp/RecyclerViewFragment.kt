d03547dcc3736a1edc7f15576380315506e3ad4c
Damian Soo <sooda336@student.otago.ac.nz>
2020-06-01 01:00:23 +1200

    Recycler list now filters non folders/pdfs and added a base sorting method to the files

--- app/src/main/java/com/example/myfirstapp/RecyclerViewFragment.kt
+++ app/src/main/java/com/example/myfirstapp/RecyclerViewFragment.kt
@@ -9,11 +9,10 @@ import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.fragment_item_list.*
 import java.io.File
+import kotlin.collections.ArrayList
 
 
 class RecyclerViewFragment : Fragment() {
@@ -32,7 +31,13 @@ class RecyclerViewFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         val dir = File(args.dir)
-        val list = generateList(dir.listFiles().size)
+        var size = 0
+        for (i in 0 until dir.listFiles().size) {
+            if ((dir.listFiles()[i].extension == "pdf") or dir.listFiles()[i].isDirectory) {
+                size++
+            }
+        }
+        val list = generateList(size)
 
         recycler_view.adapter = MyItemRecyclerViewAdapter(list) { item ->
             openFile(item)
@@ -43,14 +48,20 @@ class RecyclerViewFragment : Fragment() {
 
     private fun generateList(size: Int): List<TestItem> {
 
-        val list = ArrayList<TestItem>()
         val dir = File(args.dir)
+        val filteredDir = ArrayList<File>()
+        for (i in 0 until dir.listFiles().size) {
+            if ((dir.listFiles()[i].extension == "pdf") or dir.listFiles()[i].isDirectory) {
+                filteredDir += dir.listFiles()[i]
+            }
+        }
+        val list = ArrayList<TestItem>()
 
         for (i in 0 until size) {
-            val fileType = if (dir.listFiles()[i].isDirectory) {
+            val fileType = if (filteredDir[i].isDirectory) {
                 "folder"
             } else {
-                dir.listFiles()[i].extension
+                filteredDir[i].extension
             }
 
             val drawable = when (fileType) {
@@ -59,11 +70,12 @@ class RecyclerViewFragment : Fragment() {
                 else -> R.drawable.ic_thumb
             }
 
-            val item = TestItem(drawable, dir.listFiles()[i].name, fileType)
+            val item = TestItem(drawable, filteredDir[i].name, fileType)
             list += item
         }
 
         return list
+        //return fileSort(list)
     }
 
     private fun openFile(item: TestItem) {
@@ -81,6 +93,10 @@ class RecyclerViewFragment : Fragment() {
                 toast.show()
             }
         }
+    }
+
+    private fun fileSort(list: ArrayList<TestItem>) : List<TestItem> {
+        return list.sortedWith(compareBy {it.text1})
     }
 }
 
