258f4fcaca894125575c9fb672838cd8c186c780
Louis Whitburn <whilo192@student.otago.ac.nz>
2020-06-05 09:18:16 +1200

    Updates .gitignore, attempted to add unit tests

--- app/src/androidTest/java/com/otago/open/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/otago/open/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.myfirstapp", appContext.packageName)
+        assertEquals("com.otago.open", appContext.packageName)
     }
 }
